293be7b1c497d75b6d175aacdac142d7bd899832
kyonsama <emanuel_raziel129730@hotmail.com>
2022-10-22 20:58:02 -0500

    clase addEventListener completa

--- CursoPracticoJavaScript/script.js
+++ CursoPracticoJavaScript/script.js
@@ -1,1 +1,27 @@
-console.log("hello, world")
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const resultado = document.querySelector('#result');
+
+
+// form.addEventListener('submit', btnOnClick);
+
+// function btnOnClick(event){
+//     event.preventDefault();
+//     const  numero1 = parseInt(input1.value);
+//     const  numero2 = parseInt(input2.value);
+//     resultado.innerText = "Resultado: " + numero1 + numero2  
+
+// }
+
+btn.addEventListener('click', btnOnClick);
+
+function btnOnClick(event){
+    // event.preventDefault();
+    const  numero1 = parseInt(input1.value);
+    const  numero2 = parseInt(input2.value);
+    resultado.innerText = "Resultado: " + numero1 + numero2  
+
+}
