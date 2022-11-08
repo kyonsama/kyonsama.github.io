4ba8d2b61ec8d4396ef65762d278329b86dac69e
kyonsama <emanuel_raziel129730@hotmail.com>
2022-11-07 20:21:54 -0600

    clase 2 terminada menus

--- Curso-Practico-JavaScript/scripts.js
+++ Curso-Practico-JavaScript/scripts.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar__email');
 const desktopMenu = document.querySelector('.Desktop-menu');
+const menuHamIcon = document.querySelector('.navbar--mobile__icon');
+const mobileMenu =document.querySelector('.mobile-menu'); 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
