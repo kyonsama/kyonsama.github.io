c33af972412d5e3944233cdabd23a3dd60f8a35a
kyonsama <emanuel_raziel129730@hotmail.com>
2022-11-08 21:52:58 -0600

    Terminados carrito de compras con menus js

--- Curso-Practico-JavaScript/scripts.js
+++ Curso-Practico-JavaScript/scripts.js
@@ -1,15 +1,42 @@
 const menuEmail = document.querySelector('.navbar__email');
 const desktopMenu = document.querySelector('.Desktop-menu');
 const menuHamIcon = document.querySelector('.navbar--mobile__icon');
-const mobileMenu =document.querySelector('.mobile-menu'); 
+const mobileMenu = document.querySelector('.mobile-menu'); 
+const onCarList = document.querySelector('.in-shop-car__container');
+const carIcon = document.querySelector('.navbar--mobile__icon--car');
+const carIconDesktop = document.querySelector('.navbar--right__icon');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+carIcon.addEventListener('click', toggleCarListMenu);
+carIconDesktop.addEventListener('click',toggleCarListMenu)
 
 function toggleDesktopMenu(){
+    const desktopMenuClose =  desktopMenu.classList.contains('inactive');
     desktopMenu.classList.toggle('inactive');
+
+    if (desktopMenuClose){
+        onCarList.classList.add('inactive');
+    }
+
 }
 
 function toggleMobileMenu(){
+    const mobileMenuClose = mobileMenu.classList.contains('inactive');
     mobileMenu.classList.toggle('inactive');
+
+    if (mobileMenuClose){
+        onCarList.classList.add('inactive');
+
+    }
+}
+
+function toggleCarListMenu(){
+    const onCarListClose =  onCarList.classList.contains('inactive');
+    onCarList.classList.toggle('inactive');
+
+    if (onCarListClose) {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add ('inactive');
+    }
 }
